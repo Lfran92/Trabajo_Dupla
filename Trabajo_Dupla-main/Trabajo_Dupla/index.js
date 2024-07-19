4ce361e3f71a70aa9ac046811a799d49be7a013f
franco <francodavid3692@gmail.com>
2024-07-19 12:50:35 -0300

    modificacion

--- Trabajo_Dupla-main/Trabajo_Dupla/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('searchForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var query = document.getElementById('searchInput').value;
-    window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(query);
-  });
-  
